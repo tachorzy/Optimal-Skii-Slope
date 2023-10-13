6e7cff65d157e675770c28c910545651f8434136
tachorzy <tachorzyad@gmail.com>
2023-10-13 07:09:05 -0500

    Removed runtime errors

--- optimalskiislope.py
+++ optimalskiislope.py
@@ -12,18 +12,24 @@ def bestPath(matrix, NUMBER_OF_ROWS, NUMBER_OF_COLS, pace):
         lookup[row][0] = matrix[row][0]
 
         if row == NUMBER_OF_ROWS and currentCol == NUMBER_OF_COLS:
-            return (steps, score//5)
+            return score
         if lookup[row][currentCol] == 'F':
            score += 5
            steps += 1
            row += 1
-        else: 
+        elif currentCol != NUMBER_OF_COLS-1: 
             if lookup[row][currentCol+1] == 'O':
                steps += 3
             if lookup[row][currentCol+1] == '.':
                 steps += 1
             currentCol += 1
-
+        elif row != NUMBER_OF_ROWS-1:
+            if lookup[row+1][currentCol] == 'O':
+               steps += 3
+            if lookup[row+1][currentCol] == '.':
+                steps += 1
+            row += 1
+            
 def main():
     n = int(input())
     pace = int(input())
