8490cdd85951a49738990429611f46200742ebed
tachorzy <tachorzyad@gmail.com>
2023-10-13 07:04:19 -0500

    Tentative solution

--- optimalskiislope.py
+++ optimalskiislope.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python
 import sys
 
-def bestPath(matrix, NUMBER_OF_ROWS, NUMBER_OF_COLS, initial_row=0, initial_col=0):
-    score, steps = 0
-
-    if initial_row == NUMBER_OF_ROWS and initial_col == NUMBER_OF_COLS:
-        return (steps, score//5)
+def bestPath(matrix, NUMBER_OF_ROWS, NUMBER_OF_COLS, pace):
+    score = 0 
+    steps = 0
     
     lookup = [[0 for i in range(0, NUMBER_OF_COLS)] for j in range(NUMBER_OF_ROWS)]
 
-    for i in range(0, NUMBER_OF_COLS):
-        lookup[i][0] = matrix[i][0]
+    currentCol = 0
+    for row in range(0, NUMBER_OF_COLS):
+        lookup[row][0] = matrix[row][0]
 
+        if row == NUMBER_OF_ROWS and currentCol == NUMBER_OF_COLS:
+            return (steps, score//5)
+        if lookup[row][currentCol] == 'F':
+           score += 5
+           steps += 1
+           row += 1
+        else: 
+            if lookup[row][currentCol+1] == 'O':
+               steps += 3
+            if lookup[row][currentCol+1] == '.':
+                steps += 1
+            currentCol += 1
 
 def main():
-  m, n = (int(x) for x in input().split())
-  matrix = [[int(x) for x in input().split()] for _ in range(m)]
-  print(bestPath(matrix, m, n))
+    n = int(input())
+    pace = int(input())
+    matrix = [[x for x in input().split()] for _ in range(n)]
+    print(bestPath(matrix, n, n, pace))
 
 if __name__ == "__main__":
   main()
